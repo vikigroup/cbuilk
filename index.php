369a48961bacf39e67b240e36c5372293602e54f
vikigroup <vikigroup2015@gmail.com>
2015-07-02 09:11:54 +0700

    Edited head

--- index.php
+++ index.php
@@ -53,7 +53,8 @@ if($frame!="login" && $frame!="register" && $frame!="changepass" && $frame!="cha
 
 require("module/box_device.php");
 ?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "hrvp://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="hrvp://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
