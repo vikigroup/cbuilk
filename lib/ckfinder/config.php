da086120393bbaef48f1db9b397002bdcc8e4d78
vikigroup <vikigroup2015@gmail.com>
2015-07-03 21:15:10 +0700

    Edited text editor

--- lib/ckfinder/config.php
+++ lib/ckfinder/config.php
@@ -335,7 +335,6 @@ $config['XSendfile'] = false;
 include_once "plugins/imageresize/plugin.php";
 include_once "plugins/fileeditor/plugin.php";
 include_once "plugins/zip/plugin.php";
-include_once "plugins/batch/plugin.php";
 
 $config['plugin_imageresize']['smallThumb'] = '90x90';
 $config['plugin_imageresize']['mediumThumb'] = '120x120';
